9fb3c5a4707313071e2741b39963fb90d37477eb
MatijaPejic <matija.pejic@yahoo.com>
2020-05-07 08:37:15 +0000

    Upload New File

---
+++ handler.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Podaci koji vam stizu sa stranice za rezervaciju su u JSON formatu
+Imena polja su (imena nekih polja su van konteksta) : 
+first_name, 
+last_name, 
+department (ovo je ime restorana), 
+user_password (broj mesta),
+email,
+contact_no(broj telefona)
+*/
+
+
+//TEST
+/*
+$myfile = fopen("logger.txt", "a");
+fwrite($myfile, "Ime: ".$_REQUEST['first_name']."\r\n");
+fwrite($myfile, "Prezime: ".$_REQUEST['last_name']."\r\n");
+fwrite($myfile, "Restoran: ".$_REQUEST['department']."\r\n");
+fwrite($myfile, "Broj Mesta: ".$_REQUEST['user_password']."\r\n");
+fwrite($myfile, "Email: ".$_REQUEST['email']."\r\n");
+fwrite($myfile, "Kontakt Tel.: ".$_REQUEST['contact_no']."\r\n");
+fwrite($myfile, "________________"."\r\n");
+fclose($myfile);
+*/
+
+//Povratak na stranicu za rezervaciju (ovaj deo ne dirati)
+header("Refresh:0; url=index.html");
+?>
