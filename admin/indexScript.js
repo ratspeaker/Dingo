e670c8ac7167f652a9da3cf2a81ab4a436ddb5ff
Dara Milojkovic <milojkovic.dara@gmail.com>
2020-06-15 18:28:29 +0000

    neke izmene: korisnik ne sme da brute force-om uđe u admin stranu

--- admin/indexScript.js
+++ admin/indexScript.js
@@ -1,24 +1,29 @@
 $(document).ready(function() {
-
+    var counter = 3;
     $('#forma').on("submit", function(e) {
+        if (counter == 0) {
+            alert("Iskoristili ste broj pokušaja. Vratite se malo kasnije i pokušajte ponovo.");
 
-        $.ajax({
-            dataType: 'json',
-            crossDomain: true,
-            data: {
-                ime: document.getElementById("ime").value,
-                lozinka: document.getElementById("lozinka").value
-            },
-            type: 'GET',
-            url: 'check_password.php'
-        }).done(function(data) {
-            
-            document.getElementById("form_div").hidden = true;
-            document.getElementById("buttons_div").hidden = false;
-            
-        }).fail(function() {
-             alert("Pogrešna šifra ili korisničko ime. Molimo Vas unesite ispravne podatke!");
-        });
+        } else {
+            counter = counter - 1;
+            $.ajax({
+                dataType: 'json',
+                crossDomain: true,
+                data: {
+                    ime: document.getElementById("ime").value,
+                    lozinka: document.getElementById("lozinka").value
+                },
+                type: 'GET',
+                url: 'check_password.php'
+            }).done(function(data) {
+
+                document.getElementById("form_div").hidden = true;
+                document.getElementById("buttons_div").hidden = false;
+
+            }).fail(function() {
+                alert("Pogrešna šifra ili korisničko ime. Molimo Vas unesite ispravne podatke!\nBroj pokušaja: " + counter);
+            });
+        }
         e.preventDefault();
 
     });
